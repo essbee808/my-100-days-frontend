7caf122b6405bdae20daaa06ab5514950cca32b8
Sheena Bonilla <sheena.myb@gmail.com>
2022-06-21 13:06:38 -0700

    Create entry class and constructor

---
+++ src/entry.js
@@ -0,0 +1,11 @@
+class Entry{
+    constructor(gratitude, overall_goal, highlights, struggles, solutions, final_thoughts, user_id){
+        this.gratitude = gratitude;
+        this.overall_goal = overall_goal;
+        this.highlights = highlights;
+        this.struggles = struggles;
+        this.solutions = solutions;
+        this.final_thoughts = final_thoughts;
+        this.user_id = user_id;
+    }
+}
