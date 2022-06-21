6f0fe31ae39fe273475e646e449fffb386fa5f42
Sheena Bonilla <sheena.myb@gmail.com>
2022-06-21 13:19:02 -0700

    Add entry form

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Dom fully loaded and parsed!')
+    createEntry();
+})
+
 const base_url = "http://127.0.0.1:3000"
 
+function createEntry() {
+    let entryForm = document.getElementById("entry-form")
+
+    entryForm.innerHTML += `
+        <form>
+            <h4>I am grateful for:</h4>
+            <input type="text" id="gratitude"><br>
+
+            <h4>I am grateful for:</h4>
+            <input type="text" id="gratitude"><br>
+            <input type="submit" value="Add Entry">
+        </form>
+    `
+}
